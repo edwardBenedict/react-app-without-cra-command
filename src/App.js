9bd29ba0e9197f6e3689ebea3d00184b14792abd
Edward <edwardbenedict92@gmail.com>
2020-12-17 14:11:16 +0300

    add main files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import unique from "./unique.jpg";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="one">Ed Benedict Project</h1>
+      {/* <img src={unique} alt="unique" /> */}
+    </div>
+  );
+};
+
+export default App;
